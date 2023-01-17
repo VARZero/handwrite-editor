b8cec47a4afbdf45810ab8ca3fd7d676d5520035
신성민 <cydiazero@gmail.com>
2023-01-17 23:17:34 +0900

    js 작성시작

---
+++ example/js/script.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("LineText").onclick = function() {
+    window.alert(this.id);
+};
+
+document.getElementsByClassName("LineSepr").onclick = function() {
+    window.alert(this.id);
+};
