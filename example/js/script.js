1d8c411da21ae2e9d762d97d8d25a4143e49582f
신성민 <cydiazero@gmail.com>
2023-01-17 23:36:18 +0900

    변경

--- example/js/script.js
+++ example/js/script.js
@@ -1,7 +1,7 @@
 document.getElementsByClassName("LineText").onclick = function() {
-    window.alert(this.id);
+    console.log(this.id);
 };
 
 document.getElementsByClassName("LineSepr").onclick = function() {
-    window.alert(this.id);
+    console.log(this.id);
 };
