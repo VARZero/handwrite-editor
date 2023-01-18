7212f37e7d6d34e3d53769c1a8a7f71b49a1f1a6
신성민 <cydiazero@gmail.com>
2023-01-18 23:29:33 +0900

    js 작성시작

--- example/js/script.js
+++ example/js/script.js
@@ -1,7 +1,16 @@
-document.getElementsByClassName("LineText").onclick = function() {
-    console.log(this.id);
-};
-
-document.getElementsByClassName("LineSepr").onclick = function() {
-    console.log(this.id);
-};
+function LineClick(event){
+    line = event.currentTarget
+    El = event.Target;
+    console.log(line); console.log(El);
+    NumEl = document.querySelector("#"+line+".OneNum")
+    if (El.className == "LineText"){
+        El.style.height = "calc(var(--textsize) * 5);";
+        El.style.fontSize = "calc(var(--textsize) * 1.5);";
+        El.style.fontFamily = "font-family: 'Shadows Into Light';";
+        NumEl.style.height = "calc(var(--textsize) * 5.4);";
+    }
+    else if(El.className == "LineSepr"){
+        El.style.height = "calc(var(--textsize) * 3.8);";
+        NumEl.style.height = "calc(var(--textsize) * 4.4);";
+    }
+}
