136bc13d02bda40edcaa05e680e72cc52f08d96d
신성민 <cydiazero@gmail.com>
2023-02-05 16:50:48 +0900

    호버 부분 추가

--- ex2/js/script.js
+++ ex2/js/script.js
@@ -1,6 +1,7 @@
 let fontsize = Number(getComputedStyle(document.documentElement).getPropertyValue("--fontsize").replace(/[^0-9]/g,""));
 let maxDL;
 let FileLineEnd = 1, ScrollFirstLine = 1;
+let Texts = [];
 
 function OneTextSize(){
     return document.getElementById("#textsize").offsetWidth;
@@ -14,12 +15,13 @@ function FileReadtoLocal(){
     
 }
 
+
 function DisplayTextLines(DLStart){
     // 지금까지 작성된 코드 내보내기 부분 필요
     editor.removeChild();
     for (Line=1; ((Line+DLStart-1)<=maxDL || (Line+DLStart-1)!=FileLineEnd); Line+=1){
         Oline = OneLine.cloneNode(); Oline.id += String(Line);
-        // 파일에서 한줄 가져오는 코드 추가 필요
+
         editor.appendChild(Oline);
     }
 }
